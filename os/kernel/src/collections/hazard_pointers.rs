386dacee31c73be2d6d912ced6a4129621542487
Lazar Konstantinou <LordLazor@gmx.de>
2026-09-16 11:01:27 +0200

    Set K to 4 for Join Map's List of Lists

--- os/kernel/src/collections/hazard_pointers.rs
+++ os/kernel/src/collections/hazard_pointers.rs
@@ -21,10 +21,9 @@ fn r_threshold() -> usize {
 // K: the number of hazard pointers each thread uses
 // Note: K is a constant that can be chosen at compile time
 // For the Lock-Free List implementation, you only need K=2 hazard pointers per thread
-// For the Join Map we need K=3 as we need 2 HP's for the List and 1 HP for the Stack of Joiners.
-// I've therefore have chosen K=3 
-// In the future you may adapt this to not waste memory for the lock-free list implementation
-const K: usize = 3;
+// For the join_map for example you need K=4 hazard pointers
+// 2 for the outer list and 2 for the inner list
+const K: usize = 4;
 
 // Hazard pointer record
 // structure HPRecType { HP[K]: *NodeType; Next: *HPRecType; }
